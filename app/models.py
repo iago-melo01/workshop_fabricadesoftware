85b4ab1914dfc571826d4fba092fbd4f34f8ade5
Iago Veras <ivdm0406@gmail.com>
2025-09-09 16:37:59 -0300

    Criado a model user e migrações feitas

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField(max_length=100)
+    idade = models.IntegerField()
+    email = models.EmailField(unique="true")
+    
+    def __str__(self):
+        return self.nome
+    
